cee16f560186df5a99144f24ec5d92eeff38e4be
salamaddin <salamaddinsk100@gmail.com>
2024-03-02 17:54:35 +0530

    code updated

---
+++ AutomationFramwork/src/main/java/Lib/Files/User.java
@@ -0,0 +1,41 @@
+package Lib.Files;
+
+public class User {
+
+	private String env,userRole,userName,userPassword;
+
+	public String getEnv() {
+		return env;
+	}
+
+	public void setEnv(String env) {
+		this.env = env;
+	}
+
+	public String getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}	
+
+}
