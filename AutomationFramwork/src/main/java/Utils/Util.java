606e837d66cdafcae55262a257cdad16538105cc
salamaddin <salamaddinsk100@gmail.com>
2024-03-06 23:49:37 +0530

    Code updated

---
+++ AutomationFramwork/src/main/java/Utils/Util.java
@@ -0,0 +1,57 @@
+package Utils;
+
+import java.io.InputStream;
+
+public class Util {
+	
+	public static void hold(int milliseconds) {
+		try {
+			Thread.sleep(milliseconds);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public String randomString() {
+		return randomString(5);
+	}
+	public String randomString(int n) {
+		
+		String Alphabets = "abcdefghijklmnopqrstuvxyz";
+
+		String result = "";
+		for (int i = 0; i < n; i++) {
+
+			int index = (int) (Alphabets.length() * Math.random());
+
+			result += Alphabets.charAt(index);
+		}
+		
+		return result;
+		}
+	
+	public  InputStream getResourceAsIS(String fileName){
+		try {
+			return this.getClass().getClassLoader().getResourceAsStream(fileName);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
+	public  String getResourcePath(String fileName){
+		String path = "";
+		try {
+			 path =  this.getClass().getClassLoader().getResource(fileName).getPath();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return path;
+	}
+	
+	
+}
